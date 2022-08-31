c0500db5e6fbb1046d4da425afe08ff6065a8486
Radosław Kuzyk <re.kuzyk@gmail.com>
2022-08-31 21:56:54 +0200

    Removed functions that were reimplemented in components.

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -6,44 +6,8 @@ export function delay(milliseconds){
       downloaded from:
       https://alvarotrigo.com/blog/wait-1-second-javascript/
     */
-    return new Promise(resolve => {
-        setTimeout(resolve, milliseconds);
-    });
+    return new Promise(resolve => setTimeout(resolve, milliseconds));
 }
-
-export function getOnChange(update, apiEndpoint, fail = f => f,
-                            timeOut = 1000) {
-    /*
-     * Returns function for updating input text-fields in TaskDetails
-     * and related components.
-     *
-     * update - function for updating component state
-     */
-
-    const apiClient = new ApiClient(apiEndpoint);
-
-    return (key, taskId) => {
-        const handleFail = async response => {
-            if(!response.ok) {
-                fail(response);
-                const response_ = await apiClient.get(`/${taskId}`);
-                const obj = {[key]: response_.body[key]};
-                update({...obj});
-            }
-        };
-        const patch = debounce((path, _obj) => apiClient
-                               .patch(path, _obj)
-                               .then(handleFail), timeOut);
-        const getValue = ev => { return {[key]: ev.currentTarget.value}; };
-
-        return async ev => {
-            const obj = getValue(ev);
-            update({...obj});
-            patch(`${taskId}/update`, obj);
-        };
-    };
-}
-
 
 export function today(addDays = 0) {
     const date = new Date();
@@ -51,23 +15,6 @@ export function today(addDays = 0) {
     const [isoDate] = date.toISOString().split("T");
 
     return isoDate;
-}
-
-export function tickOff(taskId, apiEndpoint, onSuccess = f => f) {
-    const apiClient = new ApiClient(apiEndpoint);
-    apiClient.put(`${taskId}/tick-off`).then(onSuccess);
-}
-
-export function getChangeTaskStatus(apiEndpoint, onSuccess = f => f) {
-    /* Returns function to activate/deactivate task.*/
-
-    const apiClient = new ApiClient(apiEndpoint);
-    return taskId => apiClient.patch(`${taskId}/status`).then(onSuccess);
-}
-
-export function getResetTask(apiEndpoint, onSuccess = f => f) {
-    const apiClient = new ApiClient(apiEndpoint);
-    return taskId => apiClient.patch(`${taskId}/reset`).then(onSuccess);
 }
 
 export function sortDate(a, b) {
